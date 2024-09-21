8ef15f9e773a699e131ce3e21f96574513ea9e83
dv9patel <dv9patel@uwaterloo.ca>
2024-09-20 23:42:42 -0400

    improving parser to detect invalid input sequence

--- a1/assignment_1/main/utils.py
+++ a1/assignment_1/main/utils.py
@@ -1,12 +1,29 @@
 import re;
+from assignment_1.main.errors import *;
 
 def edgeTranspose(t):
     return (t[1],t[0]);
 
 def parser(line):
+    pattern = r"(?:add|mod|rm|gg)(?:\s*)(?:\".*\"(?:\s*))?(?:(?:\(\s*-?\d+\s*,\s*-?\d+\s*\)\s*)*)";
+    match = re.match(pattern,line);
+
+    if match is None: raise InvalidInput();
+    else:
+        st,en = match.span();
+        if st != 0 or en - st != len(line):
+            raise InvalidInput();
+
     ops = re.findall(r"^(\S+)",line);
     on = re.findall(r"(\".*\")",line);
     data = re.findall(r"(\(\s*-?\d+\s*,\s*-?\d+\s*\))",line);
+
+    if ops in ["add","mod"] and (len(on) != 1 or len(data) < 2):
+        raise InvalidInput();
+    elif ops == "rm" and (len(on) != 1 or len(data) > 0):
+        raise InvalidInput();
+    elif ops == "gg" and (len(on) != 0 or len(data) > 0):
+        raise InvalidInput();
 
     on = [_.lower() for _ in on];
 
