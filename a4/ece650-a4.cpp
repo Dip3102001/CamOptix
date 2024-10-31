b927b1857388d4a769c895b58d2060750f3ac28e
dv9patel <dv9patel@uwaterloo.ca>
2024-10-31 11:50:24 -0400

    adding minisat

--- a4/ece650-a4.cpp
+++ a4/ece650-a4.cpp
@@ -1,3 +1,19 @@
+#include<memory>
+#include "minisat/core/SolverTypes.h"
+#include "minisat/core/Solver.h"
+
+#include<iostream>
+
+#include<vector>
+#include<string>
+
+using namespace std;
+
+void solver(int V, int k, vector<pair<int,int>> G){
+    std::unique_ptr();
+}
+
 int main(int argc, char **argv) {
- return 0;
+
+    return 0;    
 }
